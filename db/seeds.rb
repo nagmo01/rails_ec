41b25ce217de3af02ec41b534c820dff6571b685
nagmo01 <bjworkshop01@gmail.com>
2023-11-29 15:23:22 +0900

    画像の配置を/storageに変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@
 
 6.times do |i|
   item = Item.create!(name: "商品#{i}", price: rand(1000..10_000), sku: "SKU#{i}", description: "商品#{i}の説明文です。")
-  item.image.attach(io: File.open(Rails.root.join('app/assets/images/ec_cart.jpg')), filename: 'ec_cart.jpg')
+  item.image.attach(io: File.open(Rails.root.join('storage/ec_cart.jpg')), filename: 'ec_cart.jpg')
 end
