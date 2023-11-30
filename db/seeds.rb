e3ccf57a03be9f0b8da5cd969ff122da446c087b
nagmo01 <bjworkshop01@gmail.com>
2023-11-30 19:31:07 +0900

    S3のファイル指定のパスを変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,5 @@
 
 6.times do |i|
   item = Item.create!(name: "商品#{i}", price: rand(1000..10_000), sku: "SKU#{i}", description: "商品#{i}の説明文です。")
-  image = open('https://s3-ap-northeast-1.amazonaws.com/nagumo-storage/cart.jpg')
-  item.image.attach(io: image, filename: 'cart.jpg')
+  item.image.attach(io: File.open('cart.jpg'), filename: 'cart.jpg')
 end
