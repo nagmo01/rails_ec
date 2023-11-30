c9482766373858fa1378f2914d8f56f8cf7b0cde
nagmo01 <bjworkshop01@gmail.com>
2023-11-30 15:00:59 +0900

    seeds.rbを編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 
 6.times do |i|
   item = Item.create!(name: "商品#{i}", price: rand(1000..10_000), sku: "SKU#{i}", description: "商品#{i}の説明文です。")
-  image = File.open('https://s3-ap-northeast-1.amazonaws.com/nagumo-storage/cart.jpg')
+  image = open('https://s3-ap-northeast-1.amazonaws.com/nagumo-storage/cart.jpg')
   item.image.attach(io: image, filename: 'cart.jpg')
 end
