30cd9bdbbf2de6cdf5f8f4a8fa8f50843d1e91bd
nagmo01 <bjworkshop01@gmail.com>
2023-12-03 00:02:14 +0900

    Rails.root.join記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@
 
 6.times do |i|
   item = Item.create!(name: "商品#{i}", price: rand(1000..10_000), sku: "SKU#{i}", description: "商品#{i}の説明文です。")
-  item.image.attach(io: File.open('app/assets/images/cart.jpg'), filename: 'cart.jpg')
+  item.image.attach(io: File.open(Rails.root.join('app/assets/images/cart.jpg')), filename: 'cart.jpg')
 end
