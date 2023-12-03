0e5e51694c787f60eae422473d32171fdaf04241
nagmo01 <bjworkshop01@gmail.com>
2023-12-03 14:34:31 +0900

    表示中の商品を関連から除外するための処理をviewからcontrollerに移した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,22 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
-
+=begin
+  def show
+    items = Item.all
+    @item = Item.find(params[:id])
+    @items = []
+    items.each do |item|
+      unless item.id == @item.id
+        @items.push(item)
+      end
+    end
+  end
+=end
   def show
     @items = Item.all
     @item = Item.find(params[:id])
+    @items = @items.reject { |x| x.id == @item.id }
   end
+
 end
