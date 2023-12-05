9df390b7805dbdacedaaa948e36cd0deb08c222a
nagmo01 <bjworkshop01@gmail.com>
2023-12-05 14:35:20 +0900

    adminページのルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'admin/index'
+
+  get "/admin" => "admin#index"
+  get "/admin/new" => "admin#new"
+  get "/admin/:id" => "admin#show"
+  post "/admin" => "admin#create"
+  get "/admin/edit/:id" => "admin#edit"
+  patch "/admin/update/:id" => "admin#update"
+  delete "/admin/:id" => "admin#destroy"
+
 
   get '/' => 'items#index'
   get '/items/:id' => 'items#show'
