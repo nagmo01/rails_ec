c42c32b49b7dfec2fc792729efdd14893bcb6dac
nagmo01 <bjworkshop01@gmail.com>
2023-12-07 00:33:06 +0900

    rubocop

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AdminController < ApplicationController
   before_action :basic_auth
   def index
@@ -12,32 +14,30 @@ class AdminController < ApplicationController
     @item = Item.new(item_params)
 
     if @item.save
-      flash[:success] = "商品を登録しました。"
-      redirect_to "/admin"
+      flash[:success] = '商品を登録しました。'
+      redirect_to '/admin'
     else
-      flash[:danger] = "商品の登録に失敗しました。もう一度試してください。"
+      flash[:danger] = '商品の登録に失敗しました。もう一度試してください。'
       render 'new', status: :unprocessable_entity
     end
   end
-  
-
 
   def edit
     @item = Item.find(params[:id])
-    if @item.nil?
-      flash[:danger] = "商品が見つかりませんでした。"
-      redirect_to "/admin"
-    end
+    return unless @item.nil?
+
+    flash[:danger] = '商品が見つかりませんでした。'
+    redirect_to '/admin'
   end
 
   def update
     @item = Item.find(params[:id])
-  
+
     if @item.update(item_params)
-      flash[:success] = "商品を更新しました。"
-      redirect_to "/admin"
+      flash[:success] = '商品を更新しました。'
+      redirect_to '/admin'
     else
-      flash[:danger] = "商品の更新に失敗しました。もう一度試してください。"
+      flash[:danger] = '商品の更新に失敗しました。もう一度試してください。'
       render :edit
     end
   end
@@ -45,21 +45,19 @@ class AdminController < ApplicationController
   def destroy
     @item = Item.find(params[:id])
     @item.destroy
-    flash[:success] = "投稿が削除されました。"
-    redirect_to "/admin", status: :see_other
+    flash[:success] = '投稿が削除されました。'
+    redirect_to '/admin', status: :see_other
   end
 
-private
-  
+  private
+
   def item_params
     params.require(:item).permit(:name, :price, :sku, :description, :image)
   end
-  
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
-  get "/admin" => "admin#index"
-  get "/admin/new" => "admin#new"
-  get "/admin/:id" => "admin#show"
-  post "/admin" => "admin#create"
-  get "/admin/:id/edit" => "admin#edit"
-  patch "/admin/:id" => "admin#update"
-  delete "/admin/:id" => "admin#destroy"
-
+  get '/admin' => 'admin#index'
+  get '/admin/new' => 'admin#new'
+  get '/admin/:id' => 'admin#show'
+  post '/admin' => 'admin#create'
+  get '/admin/:id/edit' => 'admin#edit'
+  patch '/admin/:id' => 'admin#update'
+  delete '/admin/:id' => 'admin#destroy'
 
   get '/' => 'items#index'
   get '/items/:id' => 'items#show'
