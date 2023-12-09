bdeb6966f828f1f968daefb2ee35422b0ebe097b
nagmo01 <bjworkshop01@gmail.com>
2023-12-10 01:30:58 +0900

    rubocop&htmlbeautilier

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,63 +1,65 @@
 # frozen_string_literal: true
 
-class Admin::ItemsController < ApplicationController
-  before_action :basic_auth
-  def index
-    @items = Item.all.order(created_at: :asc)
-  end
-
-  def new
-    @item = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-
-    if @item.save
-      flash[:success] = '商品を登録しました。'
-      redirect_to admin_items_path
-    else
-      flash[:danger] = '商品の登録に失敗しました。もう一度試してください。'
-      render 'new', status: :unprocessable_entity
+module Admin
+  class ItemsController < ApplicationController
+    before_action :basic_auth
+    def index
+      @items = Item.all.order(created_at: :asc)
     end
-  end
 
-  def edit
-    @item = Item.find(params[:id])
-    return unless @item.nil?
-
-    flash[:danger] = '商品が見つかりませんでした。'
-    redirect_to '/admin'
-  end
-
-  def update
-    @item = Item.find(params[:id])
-
-    if @item.update(item_params)
-      flash[:success] = '商品を更新しました。'
-      redirect_to admin_items_path
-    else
-      flash[:danger] = '商品の更新に失敗しました。もう一度試してください。'
-      render :edit
+    def new
+      @item = Item.new
     end
-  end
 
-  def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-    flash[:success] = '投稿が削除されました。'
-    redirect_to admin_items_path, status: :see_other
-  end
+    def create
+      @item = Item.new(item_params)
 
-  private
+      if @item.save
+        flash[:success] = '商品を登録しました。'
+        redirect_to admin_items_path
+      else
+        flash[:danger] = '商品の登録に失敗しました。もう一度試してください。'
+        render 'new', status: :unprocessable_entity
+      end
+    end
 
-  def item_params
-    params.require(:item).permit(:name, :price, :sku, :description, :image)
-  end
+    def edit
+      @item = Item.find(params[:id])
+      return unless @item.nil?
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+      flash[:danger] = '商品が見つかりませんでした。'
+      redirect_to '/admin'
+    end
+
+    def update
+      @item = Item.find(params[:id])
+
+      if @item.update(item_params)
+        flash[:success] = '商品を更新しました。'
+        redirect_to admin_items_path
+      else
+        flash[:danger] = '商品の更新に失敗しました。もう一度試してください。'
+        render :edit
+      end
+    end
+
+    def destroy
+      @item = Item.find(params[:id])
+      @item.destroy
+      flash[:success] = '投稿が削除されました。'
+      redirect_to admin_items_path, status: :see_other
+    end
+
+    private
+
+    def item_params
+      params.require(:item).permit(:name, :price, :sku, :description, :image)
+    end
+
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+      end
     end
   end
 end
