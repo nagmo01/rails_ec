5d69d0943512c18373f25facd999d57a292cf064
nagmo01 <bjworkshop01@gmail.com>
2023-12-28 02:58:19 +0900

    購入処理をするcreateアクションを定義

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,10 +4,11 @@ class PaymentsController < ApplicationController
 
     if @payment.save
       flash[:success] = "購入ありがとうございます"
-      redirect_to items_path
+      redirect_to root_path
     else
       flash[:danger] = "購入に失敗しました"
       @total_price = 0
+      @cart = Cart.find_by(id: session[:cart_id])
       @cart.cart_items.each do |cart_item|
         @total_price += cart_item.item.price * cart_item.quantity
       end
@@ -21,7 +22,7 @@ class PaymentsController < ApplicationController
   private
   
   def payment_params
-    params.require(:payment).permit(:last_name, :first_name, :user_name, :email, :address, :address2, :country, :state, :zip, :cc_name, :cc_number, :cc_expiration, :cc_cvv)
+    params.require(:payment).permit(:last_name, :first_name, :user_name, :email, :address, :address2, :pref, :city, :zip, :cc_name, :cc_number, :cc_expiration, :cc_cvv)
   end
 
 end
