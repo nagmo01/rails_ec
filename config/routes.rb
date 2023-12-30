fdfc7de79f279308b79b53d027d26450d3332050
nagmo01 <bjworkshop01@gmail.com>
2023-12-30 16:46:11 +0900

    チェックアウト処理作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'payments/create'
+  
   namespace :admin do
     resources :items
+    resources :purchased_items
   end
 
   root 'items#index'
-  # get '/admin' => 'admin#index'
-  # get '/admin/new' => 'admin#new'
-  # get '/admin/:id' => 'admin#show'
-  # post '/admin' => 'admin#create'
-  # get '/admin/:id/edit' => 'admin#edit'
-  # patch '/admin/:id' => 'admin#update'
-  # delete '/admin/:id' => 'admin#destroy'
+  get '/items/:id' => 'items#show'
 
   post '/cart/:id' => 'cart_items#create'
   get '/cart' => 'cart_items#index'
   delete '/cart/:id' => 'cart_items#destroy'
 
+  get 'payments/create'
   post '/payment' => 'payments#create'
 
-  # get '/' => 'items#index'
-  get '/items/:id' => 'items#show'
+
 end
