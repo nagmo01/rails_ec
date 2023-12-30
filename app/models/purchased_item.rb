f62c3fa6a3a80f653a054a9c7fe4822ab031273e
nagmo01 <bjworkshop01@gmail.com>
2023-12-30 16:44:03 +0900

    購入済商品のモデル、テーブルを作成

---
+++ app/models/purchased_item.rb
@@ -0,0 +1,3 @@
+class PurchasedItem < ApplicationRecord
+  belongs_to :payment
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_34_045809) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_34_045810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,8 +84,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_34_045809) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "purchased_items", force: :cascade do |t|
+    t.bigint "payment_id"
+    t.string "name"
+    t.integer "price"
+    t.text "description"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["payment_id"], name: "index_purchased_items_on_payment_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "items"
+  add_foreign_key "purchased_items", "payments"
 end
