ea16a0f3e5580c0f75e9ce51bfa0baa30253a5e4
nagmo01 <bjworkshop01@gmail.com>
2024-01-01 03:02:43 +0900

    購入済み商品の保存に失敗した場合ロールバックを行う

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,53 +5,51 @@ class PaymentsController < ApplicationController
     @payment = Payment.new(payment_params)
     @cart = Cart.find_by(id: session[:cart_id])
 
-    begin
+    if @payment.save
       ActiveRecord::Base.transaction do
-        @payment.save!
         create_purchased_items
         CheckoutMailer.complete(@payment).deliver
         clear_cart
         flash[:success] = '購入ありがとうございます'
         redirect_to root_path
       end
-    rescue StandardError
+    else
       flash[:danger] = '購入に失敗しました'
       @total_price = calculate_total_price
       @messages = @payment.errors
       render template: 'cart_items/index', status: :unprocessable_entity
     end
-  end
 
-  private
+    private
 
-  def create_purchased_items
-    @cart.cart_items.each do |cart_item|
-      @purchased_item = PurchasedItem.new(
-        payment_id: @payment.id,
-        name: cart_item.item.name,
-        price: cart_item.item.price,
-        description: cart_item.item.description,
-        quantity: cart_item.quantity
-      )
-      @purchased_item.save!
+    def create_purchased_items
+      @cart.cart_items.each do |cart_item|
+        @purchased_item = PurchasedItem.new(
+          payment_id: @payment.id,
+          name: cart_item.item.name,
+          price: cart_item.item.price,
+          description: cart_item.item.description,
+          quantity: cart_item.quantity
+        )
+        @purchased_item.save!
+      end
+    end
+
+    def calculate_total_price
+      total_price = 0
+      @cart.cart_items.each do |cart_item|
+        total_price += cart_item.item.price * cart_item.quantity
+      end
+      total_price
+    end
+
+    def clear_cart
+      session[:cart_id] = nil
+      @cart.destroy
+    end
+
+    def payment_params
+      params.require(:payment).permit(:last_name, :first_name, :user_name, :email, :address, :address2, :pref, :city,
+                                      :zip, :cc_name, :cc_number, :cc_expiration, :cc_cvv)
     end
   end
-
-  def calculate_total_price
-    total_price = 0
-    @cart.cart_items.each do |cart_item|
-      total_price += cart_item.item.price * cart_item.quantity
-    end
-    total_price
-  end
-
-  def clear_cart
-    session[:cart_id] = nil
-    @cart.destroy
-  end
-
-  def payment_params
-    params.require(:payment).permit(:last_name, :first_name, :user_name, :email, :address, :address2, :pref, :city,
-                                    :zip, :cc_name, :cc_number, :cc_expiration, :cc_cvv)
-  end
-end
