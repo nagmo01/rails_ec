ecd4e58135d2a0db26c19a8dd5ab16c8d01a2dd8
nagmo01 <bjworkshop01@gmail.com>
2024-01-01 16:02:47 +0900

    購入済みアイテムの保存に失敗したときにロールバックし例外処理を発生させる

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,51 +5,58 @@ class PaymentsController < ApplicationController
     @payment = Payment.new(payment_params)
     @cart = Cart.find_by(id: session[:cart_id])
 
-    if @payment.save
+    begin
       ActiveRecord::Base.transaction do
-        create_purchased_items
-        CheckoutMailer.complete(@payment).deliver
-        clear_cart
-        flash[:success] = '購入ありがとうございます'
-        redirect_to root_path
+        if @payment.save
+          create_purchased_items
+          CheckoutMailer.complete(@payment).deliver
+          clear_cart
+          flash[:success] = '購入ありがとうございます'
+          redirect_to root_path
+        else
+          flash[:danger] = '購入に失敗しました'
+          @total_price = calculate_total_price
+          @messages = @payment.errors
+          render template: 'cart_items/index', status: :unprocessable_entity
+        end
       end
-    else
-      flash[:danger] = '購入に失敗しました'
-      @total_price = calculate_total_price
-      @messages = @payment.errors
-      render template: 'cart_items/index', status: :unprocessable_entity
-    end
-
-    private
-
-    def create_purchased_items
-      @cart.cart_items.each do |cart_item|
-        @purchased_item = PurchasedItem.new(
-          payment_id: @payment.id,
-          name: cart_item.item.name,
-          price: cart_item.item.price,
-          description: cart_item.item.description,
-          quantity: cart_item.quantity
-        )
-        @purchased_item.save!
-      end
-    end
-
-    def calculate_total_price
-      total_price = 0
-      @cart.cart_items.each do |cart_item|
-        total_price += cart_item.item.price * cart_item.quantity
-      end
-      total_price
-    end
-
-    def clear_cart
-      session[:cart_id] = nil
-      @cart.destroy
-    end
-
-    def payment_params
-      params.require(:payment).permit(:last_name, :first_name, :user_name, :email, :address, :address2, :pref, :city,
-                                      :zip, :cc_name, :cc_number, :cc_expiration, :cc_cvv)
+    rescue StandardError
+      clear_cart
+      flash[:danger] = '購入処理に失敗しました、最初からやり直してください。'
+      redirect_to root_path
     end
   end
+
+  private
+
+  def create_purchased_items
+    @cart.cart_items.each do |cart_item|
+      @purchased_item = PurchasedItem.new(
+        payment_id: @payment.id,
+        name: cart_item.item.name,
+        price: cart_item.item.price,
+        description: cart_item.item.description,
+        quantity: cart_item.quantity
+      )
+      @purchased_item.save!
+    end
+  end
+
+  def calculate_total_price
+    total_price = 0
+    @cart.cart_items.each do |cart_item|
+      total_price += cart_item.item.price * cart_item.quantity
+    end
+    total_price
+  end
+
+  def clear_cart
+    session[:cart_id] = nil
+    @cart.destroy
+  end
+
+  def payment_params
+    params.require(:payment).permit(:last_name, :first_name, :user_name, :email, :address, :address2, :pref, :city,
+                                    :zip, :cc_name, :cc_number, :cc_expiration, :cc_cvv)
+  end
+end
